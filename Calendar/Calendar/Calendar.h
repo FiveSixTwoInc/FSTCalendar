9bed483adccda0c038ecb20cb416e0bf113018f5
Jackson Beachwood <jackson.beachwood@trafficmanagement.com>
2015-09-16 16:50:44 -0700

    Initial Commit

---
+++ Calendar/Calendar/Calendar.h
@@ -0,0 +1,19 @@
+//
+//  Calendar.h
+//  Calendar
+//
+//  Created by Jackson Beachwood on 9/16/15.
+//  Copyright © 2015 FiveSixTwo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Calendar.
+FOUNDATION_EXPORT double CalendarVersionNumber;
+
+//! Project version string for Calendar.
+FOUNDATION_EXPORT const unsigned char CalendarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calendar/PublicHeader.h>
+
+
