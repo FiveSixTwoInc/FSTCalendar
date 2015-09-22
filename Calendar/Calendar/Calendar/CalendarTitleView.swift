23f9be6d61266807b2215db0569bad7276c09c72
Jackson Beachwood <jackson.beachwood@trafficmanagement.com>
2015-09-22 12:11:45 -0700

    Added CalendarTitleView

---
+++ Calendar/Calendar/Calendar/CalendarTitleView.swift
@@ -0,0 +1,13 @@
+//
+//  CalendarTitleView.swift
+//  FSTCalendar
+//
+//  Created by Jackson Beachwood on 9/22/15.
+//  Copyright © 2015 FiveSixTwo. All rights reserved.
+//
+
+import UIKit
+
+class CalendarTitleView: UIView {
+
+}
